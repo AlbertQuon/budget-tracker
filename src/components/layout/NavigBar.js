9e4104a56c63a32b4714b7c8bb3e56868f290336
Albert Quon <albertq2001@hotmail.com>
2022-08-15 21:44:15 -0400

    Added styles to navbar

--- src/components/layout/NavigBar.js
+++ src/components/layout/NavigBar.js
@@ -2,31 +2,30 @@ import { useContext } from 'react';
 import {Nav, Navbar, Container, Button} from 'react-bootstrap'
 import {NavLink} from 'react-router-dom'
 import AuthContext from '../auth/AuthContext';
+import "../../css/Navbar.css"
 
 function NavigBar() {
     const { user, logoutUser } = useContext(AuthContext);
     
     return ( 
-        <Container>
-        <Navbar expand="lg" variant="dark" bg="dark">
-            <Container>
-                <Navbar.Brand><Nav.Link as={NavLink} className="text-white" end to="/">Budget Track</Nav.Link></Navbar.Brand>
+        <Container className='custom-nav'>
+        <Navbar expand="lg" variant="dark" bg="dark" >
+            <Container className='custom-navbar'>
+                <Navbar.Brand><Nav.Link as={NavLink} className="text-white" end to="/">Budgeter</Nav.Link></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" className="justify-content-start">
                 {user ? ( <>
                 <Nav className="me-auto my-2 my-lg-0"
-                    style={{ maxHeight: '100px' }}
                     navbarScroll>
-                <Nav.Link as={NavLink} end to="/">Home</Nav.Link>
-                    <Nav.Link as={NavLink} to="budget/">Budget</Nav.Link>
-                    <Nav.Link as={NavLink} to="transactions/">Transactions</Nav.Link>
-                    <Nav.Link as={NavLink} to="settings/">Settings</Nav.Link>
+                    <Nav.Link className='custom-nav-link mx-2' as={NavLink} end to="/">Home</Nav.Link>
+                    <Nav.Link className='custom-nav-link mx-2' as={NavLink} to="budget/">Budget</Nav.Link>
+                    <Nav.Link className='custom-nav-link mx-2' as={NavLink} to="transactions/">Transactions</Nav.Link>
+                    <Nav.Link className='custom-nav-link mx-2' as={NavLink} to="settings/">Settings</Nav.Link>
                 </Nav>
                 <Button onClick={logoutUser}>Logout</Button>
                 </>  
                 ) : 
-                (  <Nav className="me-auto my-2 my-lg-0"
-                    style={{ maxHeight: '100px' }}
+                (  <Nav className="me-auto my-2 my-lg-0 customNav"
                     navbarScroll>
                     <Nav.Link as={NavLink} to="login/">Login</Nav.Link>
                     <Nav.Link as={NavLink} to="register/">Register</Nav.Link>
