f20c77d7d297257fdcb3adaf2c1ab92203d9686b
Albert Quon <albertq2001@hotmail.com>
2022-08-20 20:48:01 -0400

    Revised django settings

--- django_budget/settings.py
+++ django_budget/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'django_budget.wsgi.application'
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=50),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
