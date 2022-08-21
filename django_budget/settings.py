27dc549ccdded52d5e35e1ed0952f965c757f2a3
Albert Quon <albertq2001@hotmail.com>
2022-08-21 19:27:55 -0400

    removed unused comment

--- django_budget/settings.py
+++ django_budget/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
-        #('knox.auth.TokenAuthentication',),
     'DEFAULT_FILTER_BACKENDS':(
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
